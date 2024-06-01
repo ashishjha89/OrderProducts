66a131e28fe740d22824ec1fcf6769fbca4b7ee3
Ashish Jha <ashishjha.mymail@gmail.com>
2024-06-01 22:01:29 +0200

    Add notification-service which consumes kafka event

---
+++ notification-service/src/main/java/com/example/notification_service/NotificationServiceApplication.java
@@ -0,0 +1,31 @@
+package com.example.notification_service;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@Slf4j
+@RequiredArgsConstructor
+@SuppressWarnings("unused")
+public class NotificationServiceApplication {
+
+    private final ObservationRegistry observationRegistry;
+    private final Tracer tracer;
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @KafkaListener(topics = "notificationTopic")
+    public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Got message <{}>", orderPlacedEvent);
+        // Do some action, e.g. send out an email notification
+    }
+
+}
