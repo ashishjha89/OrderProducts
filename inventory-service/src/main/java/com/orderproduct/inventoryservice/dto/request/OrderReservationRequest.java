96d7e1b2aa1672beeb7b092f8ac22d8550dd15c0
Ashish Jha <ashishjha.mymail@gmail.com>
2025-12-25 19:15:24 +0100

    Improve formatting in inventory-service

--- inventory-service/src/main/java/com/orderproduct/inventoryservice/dto/request/OrderReservationRequest.java
+++ inventory-service/src/main/java/com/orderproduct/inventoryservice/dto/request/OrderReservationRequest.java
@@ -7,6 +7,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
 public record OrderReservationRequest(
-        @NotBlank(message = "Order number cannot be blank.") String orderNumber,
-        @NotEmpty(message = "Reservation requests cannot be empty.") @Valid List<ItemReservationRequest> itemReservationRequests) {
+                @NotBlank(message = "Order number cannot be blank.") String orderNumber,
+                @NotEmpty(message = "Reservation requests cannot be empty.") @Valid List<ItemReservationRequest> itemReservationRequests) {
 }
--- inventory-service/src/main/java/com/orderproduct/inventoryservice/dto/request/UpdateInventoryRequest.java
+++ inventory-service/src/main/java/com/orderproduct/inventoryservice/dto/request/UpdateInventoryRequest.java
@@ -6,6 +6,6 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UpdateInventoryRequest(
-                @NotBlank(message = "SKU code cannot be blank.") @Pattern(regexp = "^[a-zA-Z0-9_-]*$", message = "SKU code can only contain alphanumeric characters, hyphens, and underscores.") @Size(max = 100, message = "SKU code length must be less than 100 characters.") String skuCode,
-                @Min(value = 0, message = "Quantity must be non-negative.") int quantity) {
+        @NotBlank(message = "SKU code cannot be blank.") @Pattern(regexp = "^[a-zA-Z0-9_-]*$", message = "SKU code can only contain alphanumeric characters, hyphens, and underscores.") @Size(max = 100, message = "SKU code length must be less than 100 characters.") String skuCode,
+        @Min(value = 0, message = "Quantity must be non-negative.") int quantity) {
 }
